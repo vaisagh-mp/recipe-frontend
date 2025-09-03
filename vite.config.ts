def22f68567bcf409ea795cabd51269643c0db46
PontusDigimark <contact@pontusdigi.com>
2025-09-03 11:58:44 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
+  },
+  build: {
+    outDir: "dist",          // folder to publish
+    rollupOptions: {
+      input: "/src/main.tsx" // entry file
+    }
   }
 });
