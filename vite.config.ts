429ee796caefbb061349aa44f0e9b99d3769f778
PontusDigimark <contact@pontusdigi.com>
2025-09-03 12:10:22 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173
-  },
   build: {
-    outDir: "dist",          // folder to publish
-    rollupOptions: {
-      input: "/src/main.tsx" // entry file
-    }
-  }
+    outDir: "build", // 
+  server: {
+    port: 5173,
+  },
 });
